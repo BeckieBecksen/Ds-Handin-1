f9a836e1dd7150ea82db6f02fc41bf8c071355b0
Chris <christopher.cortsen@gmail.com>
2022-09-07 16:33:49 +0200

    First draft of functioning assignment

--- PhilosopherDin.go
+++ PhilosopherDin.go
@@ -2,31 +2,28 @@ package main
 
 import "fmt"
 
-func philo(philoId int) {
-	leftHand := make(chan int)
-	rightHand := make(chan int)
-	philoId = philoId
+func philo(philoId int, leftHand chan int, rightHand chan int) {
+
 	isEating := false
 
 	for {
 		if !isEating {
+			fmt.Println(philoId, "eating")
 			<-rightHand
 			<-leftHand
-			fmt.Println("eating")
 			isEating = true
 		} else {
 			isEating = false
-			fmt.Println("thinking")
+			fmt.Println(philoId, "thinking")
 			leftHand <- 33
 			rightHand <- 33
 		}
+
 	}
 }
 
-func fork(forkId int) {
-	isUsed := false
+func fork(forkId int, available chan int) {
 
-	available := make(chan int)
 	available <- 33
 	for {
 
@@ -34,17 +31,24 @@ func fork(forkId int) {
 }
 
 func main() {
-	go philo(1)
-	go philo(2)
-	go philo(3)
-	go philo(4)
-	go philo(5)
 
-	go fork(1)
-	go fork(2)
-	go fork(3)
-	go fork(4)
-	go fork(5)
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	ch3 := make(chan int)
+	ch4 := make(chan int)
+	ch5 := make(chan int)
+
+	go philo(1, ch1, ch2)
+	go philo(2, ch2, ch3)
+	go philo(3, ch3, ch4)
+	go philo(4, ch4, ch5)
+	go philo(5, ch5, ch1)
+
+	go fork(1, ch1)
+	go fork(2, ch2)
+	go fork(3, ch3)
+	go fork(4, ch4)
+	go fork(5, ch5)
 
 	for {
 
