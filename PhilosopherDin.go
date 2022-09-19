9393ab2f034d321533389d4ad1fa4373951afcdd
Emil Nørbjerg <emno@itu.dk>
2022-09-19 11:27:17 +0200

    Temp (#5)
    * clean up the main func
    
    The cleanup, should, not make any changes to
    the structure of the program or the output
    
    * add a delay to phi. threads
    
    By adding a slow implementation of fibonacci
    and calling it, the threads are spread out over
    time, and thus don't deadlock
    
    * change the structure of the program
    
    The goal is to get better controll of who is eating.
    
    * change go rutines away from a loop
    
    * make the philos eat
    
    By changin the order of fork-philo creation, the forks
    are ready to serve the philos once they appear
    A change to the left and right fork inputs
    made it so that the left fork was registered as the left
    and not the right

--- PhilosopherDin.go
+++ PhilosopherDin.go
@@ -5,64 +5,103 @@ import (
 	"time"
 )
 
-func philo(philoId int, leftHand, rightHand, requestLeftHand, requestRightHand chan int, eatingTime int) {
+func philo(philoId int, leftIn, rightIn chan bool, leftOut, rightOut chan int) {
 	isEating := false
 	eatCount := 1
 	for {
 		if !isEating {
-			requestLeftHand <- 22
-			requestRightHand <- 22
-			<-rightHand
-			<-leftHand
-			fmt.Println(philoId, "eating for the", eatCount, "th time" )
-			isEating = true
-			eatCount++
-			time.Sleep(time.Duration(eatingTime/2) * time.Second)
+			leftOut <- philoId
+			leftCanBeServed := <-leftIn
+			if leftCanBeServed {
+				rightOut <- philoId
+				rightCanBeServed := <-rightIn
+				if rightCanBeServed {
+					fmt.Println(philoId, "eating for the", eatCount, "th time")
+					isEating = true
+					eatCount++
+					time.Sleep(1 * time.Second)
+					leftOut <- philoId
+					rightOut <- philoId
+				} else {
+					// tell the other fork that it is free again
+					leftOut <- philoId
+				}
+			}
 		} else {
 			isEating = false
 			fmt.Println(philoId, "thinking")
-			leftHand <- 33
-			rightHand <- 33
 		}
 	}
 }
 
-func fork(forkId int, available, request chan int) {
-	inUse := false
+func fork(forkId int, leftOut, rightOut chan bool, sharedIn chan int, leftPhiloID, rightPhiloID int) {
+	isAvalible := true
+	philoUsingFork := 0
+
 	for {
-		if inUse{
-			<- available
-			inUse = false
-		}else{
-			<- request
-			inUse = true
-			available <- 33
+		philoID := <-sharedIn
+
+		if philoID == leftPhiloID {
+			if isAvalible {
+				leftOut <- true
+				philoUsingFork = philoID
+				isAvalible = false
+			} else {
+				if philoUsingFork == philoID {
+					isAvalible = true
+					philoUsingFork = 0
+				} else {
+					leftOut <- false
+				}
+			}
+		}
+
+		if philoID == rightPhiloID {
+			if isAvalible {
+				rightOut <- true
+				philoUsingFork = philoID
+				isAvalible = false
+			} else {
+				if philoUsingFork == philoID {
+					isAvalible = true
+					philoUsingFork = 0
+				} else {
+					rightOut <- false
+				}
+			}
+
+		}
+
+		if philoID != rightPhiloID && philoID != leftPhiloID {
+			fmt.Println("ERROR, on fork", forkId, "got id:", philoID, "expected one of:", leftPhiloID, rightPhiloID)
 		}
 	}
 }
 
 func main() {
-	var channelArray = [5]chan int{}
-	for i := 0; i < 5; i++ {
-		channelArray[i] = make(chan int)
-	}
-	
-	var eatingTimes = [5]int{2,3,5,7,11}
-	
-	var requestChannels = [5]chan int{}
-	for i := 0; i < 5; i++ {
-		requestChannels[i] = make(chan int)
+	var comms = [10]chan bool{}
+	for i := 0; i < 10; i++ {
+		comms[i] = make(chan (bool), 1)
 	}
 
+	var forkIn = [5]chan int{}
 	for i := 0; i < 5; i++ {
-		go fork(i+1, channelArray[i], requestChannels[i])
+		forkIn[i] = make(chan (int), 1)
 	}
 
-	for i := 0; i < 5; i++ {
-		go philo(i+1, channelArray[i], channelArray[(i+1)%5], requestChannels[i], requestChannels[(i+1)%5], eatingTimes[i])
-	}
-	
-	
+	//forkId int, leftOut, rightOut chan bool, sharedIn chan int, leftPhiloID, rightPhiloID int
+	go fork(1, comms[9], comms[0], forkIn[0], 5, 1)
+	go fork(2, comms[1], comms[2], forkIn[1], 1, 2)
+	go fork(3, comms[3], comms[4], forkIn[2], 2, 3)
+	go fork(4, comms[5], comms[6], forkIn[3], 3, 4)
+	go fork(5, comms[7], comms[8], forkIn[4], 4, 5)
+
+	//philoId int, leftIn, rightIn chan bool, leftOut, rightOut chan int
+	go philo(1, comms[0], comms[1], forkIn[0], forkIn[1])
+	go philo(2, comms[2], comms[3], forkIn[1], forkIn[2])
+	go philo(3, comms[4], comms[5], forkIn[2], forkIn[3])
+	go philo(4, comms[6], comms[7], forkIn[3], forkIn[4])
+	go philo(5, comms[8], comms[9], forkIn[4], forkIn[0])
 
 	for {
 
